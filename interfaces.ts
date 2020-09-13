5ff5b88ffe080ec4dd70d181c6036a5c1a49b42f
ovchynnikov <vapore18@gmail.com>
2020-09-13 19:03:15 +0300

    interfaces.ts

---
+++ interfaces.ts
@@ -0,0 +1,70 @@
+ interface Rect {
+   readonly id: string
+   color?: string
+   size: {
+       width: number
+       height: number
+   }
+ }
+
+ const rect1: Rect = {
+     id: '123',
+     color: '#ccc',
+     size: {
+         width: 20,
+         height: 20
+     }
+ }
+
+ const rect2: Rect = {
+     id: '1234',
+     size: {
+         width: 30,
+         height: 40
+     }
+ }
+
+ // rect2.id = '123456'  - can't do that because readonly
+ rect2.color = 'black'
+
+ const rect3 = {} as Rect
+ const rect4 = <Rect>{}
+
+ // ------
+
+ interface RectWithArea extends Rect {
+     getArea: () => number
+ }
+
+ const rect5: RectWithArea = {
+     id: '12',
+     size: {
+        width: 10,
+        height: 10
+    },
+    getArea(): number {
+        return this.size.width * this.size.height
+    }
+ }
+
+ interface IClock {
+     time: Date
+     setTime(date: Date): void
+ }
+
+ class Clock implements IClock {
+     time: Date = new Date()
+     setTime(date: Date): void {
+         this.time = date
+     }
+ }
+
+  // ------
+interface Styles {
+    [key: string]: string
+}
+
+  const css: Styles = {
+      border: '1px solid black',
+      marginTop: '2px'
+  }
