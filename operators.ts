c036ec74df3269fe6c378a596dbc9bd31d92941f
ovchynnikov <vapore18@gmail.com>
2020-09-13 18:40:20 +0300

    operators.ts

---
+++ operators.ts
@@ -0,0 +1,26 @@
+interface Person {
+    name: string
+    age: number
+}
+
+type PersonKeys = keyof Person // 'name' | 'age'
+
+let key: PersonKeys = 'name'
+key = 'age'
+
+type User = {
+    _id: number
+    name: string
+    email: string
+    createdAt: Date
+}
+
+type UserKeysNoMeta1 = Exclude<keyof User, '_id' | 'createdAt'>
+
+type UserKeysNoMeta2 = Pick<User, 'name' | 'email'>
+
+let u1: UserKeysNoMeta1 = 'name'
+// u1 = '_id'  -- can't do that
+
+let u2: UserKeysNoMeta2 = 'name' // doesn't work for some reason ???
+
