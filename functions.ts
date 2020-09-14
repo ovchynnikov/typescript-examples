10e0ed6561b0b31c8fbdc300cf024d48c02aedda
ovchynnikov <vapore18@gmail.com>
2020-09-14 16:37:11 +0300

    functions.ts

---
+++ functions.ts
@@ -0,0 +1,38 @@
+function add(a: number, b: number): number {
+    return a + b
+}
+
+console.log('Add 5 + 5: ', add(5,5))
+
+function toUpper(str: string): string {
+    return str.trim().toUpperCase()
+}
+
+interface MyPosition {
+    x: number | undefined
+    y: number | undefined
+}
+
+interface MyPositionWithDefault extends MyPosition {
+    default: string
+}
+
+function position(): MyPosition
+function position(a: number): MyPositionWithDefault
+function position(a: number, b: number): MyPosition
+
+function position(a?: number, b?: number) {
+    if (!a && !b) {
+        return { x: undefined, y: undefined}
+    }
+    if (a && !b) {
+        return {x: a, y: undefined, default: a.toString()}
+    }
+    return {x: a, y: b}
+}
+
+console.log('Empty args: ', position())
+console.log('One arg: ', position(5))
+console.log('Two args: ', position(5,5))
+
+
